6a5fcd6e8a965a2ebc12d072740d90736afd1ede
kyeon06 <jkyeon06@gmail.com>
2023-03-14 19:43:59 +0900

    :sparkles: feat : Add Album, Photo Relation

--- photoalbum/src/main/java/com/yoon/photoalbum/domain/Album.java
+++ photoalbum/src/main/java/com/yoon/photoalbum/domain/Album.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name="album", schema="photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
@@ -47,5 +48,8 @@ public class Album {
     @Column(name = "created_at", unique = false, nullable = true)
     @CreationTimestamp
     private Date createdAt;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "album", cascade = CascadeType.ALL)
+    private List<Photo> photos;
 
 }
