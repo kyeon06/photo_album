bf72b7729f1abebc809c829bbb852923d72d7d08
kyeon06 <jkyeon06@gmail.com>
2023-03-15 12:18:58 +0900

    :sparkles: feat : album 정보 조회하기 메소드 생성

---
+++ photoalbum/src/main/java/com/yoon/photoalbum/service/AlbumService.java
@@ -0,0 +1,26 @@
+package com.yoon.photoalbum.service;
+
+import com.yoon.photoalbum.domain.Album;
+import com.yoon.photoalbum.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Service
+public class AlbumService {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    public Album getAlbum(Long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if (res.isPresent()){
+            return res.get();
+        }
+        else{
+            throw new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되지 않았습니다.", albumId));
+        }
+    }
+}
