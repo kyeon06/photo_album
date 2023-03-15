edaebcabd70d9efdfbf1f990761c9594cb49c5c8
kyeon06 <jkyeon06@gmail.com>
2023-03-15 13:49:20 +0900

    :white_check_mark: Update 앨범 아이디로 조회 실패할 경우 예외처리 Test

--- photoalbum/src/test/java/com/yoon/photoalbum/service/AlbumServiceTest.java
+++ photoalbum/src/test/java/com/yoon/photoalbum/service/AlbumServiceTest.java
@@ -2,10 +2,13 @@ package com.yoon.photoalbum.service;
 
 import com.yoon.photoalbum.domain.Album;
 import com.yoon.photoalbum.repository.AlbumRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -41,5 +44,19 @@ class AlbumServiceTest {
 
         Album resAlbum = albumService.getAlbumByName(savedAlbum.getAlbumName());
         assertEquals("테스트1", resAlbum.getAlbumName());
+    }
+
+    @Test
+    void 앨범아이디_조회_예외처리() {
+        // 앨범 " 테스트 " 생성
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        album.setAlbumId(1234L);
+        Album savedAlbum = albumRepository.save(album);
+
+        // Id 조회가 실패할 경우 예외 던짐
+        Assertions.assertThrows(EntityNotFoundException.class, () -> {
+            albumService.getAlbum(12345L);
+        });
     }
 }
