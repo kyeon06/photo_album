673ccda2be84704689eac2768db896a69879eb62
kyeon06 <jkyeon06@gmail.com>
2023-08-01 23:58:51 +0900

    :white_check_mark: Add 앨범 사진 개수 조회 Test

--- photoalbum/src/test/java/com/yoon/photoalbum/service/AlbumServiceTest.java
+++ photoalbum/src/test/java/com/yoon/photoalbum/service/AlbumServiceTest.java
@@ -1,7 +1,10 @@
 package com.yoon.photoalbum.service;
 
 import com.yoon.photoalbum.domain.Album;
+import com.yoon.photoalbum.domain.Photo;
+import com.yoon.photoalbum.dto.AlbumDto;
 import com.yoon.photoalbum.repository.AlbumRepository;
+import com.yoon.photoalbum.repository.PhotoRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,6 +26,9 @@ class AlbumServiceTest {
     @Autowired
     AlbumService albumService;
 
+    @Autowired
+    PhotoRepository photoRepository;
+
     @Test
     void 앨범아이디로_앨범_정보_조회() {
 
@@ -30,7 +37,7 @@ class AlbumServiceTest {
         album.setAlbumName("테스트");
         Album savedAlbum = albumRepository.save(album);
 
-        Album resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
+        AlbumDto resAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
         assertEquals("테스트", resAlbum.getAlbumName());
     }
 
@@ -42,7 +49,7 @@ class AlbumServiceTest {
         album.setAlbumName("테스트1");
         Album savedAlbum = albumRepository.save(album);
 
-        Album resAlbum = albumService.getAlbumByName(savedAlbum.getAlbumName());
+        AlbumDto resAlbum = albumService.getAlbumByName(savedAlbum.getAlbumName());
         assertEquals("테스트1", resAlbum.getAlbumName());
     }
 
@@ -58,5 +65,20 @@ class AlbumServiceTest {
         Assertions.assertThrows(EntityNotFoundException.class, () -> {
             albumService.getAlbum(12345L);
         });
+    }
+
+    @Test
+    void 앨범_사진개수_조회() {
+        Album album = new Album();
+        album.setAlbumName("테스트");
+        Album savedAlbum = albumRepository.save(album);
+
+        // 사진 생성 -> setAlbum을 통해 앨범 지정 -> repository에 사진 저장
+        Photo photo1 = new Photo();
+        photo1.setFileName("사진1");
+        photo1.setAlbum(savedAlbum);
+        photoRepository.save(photo1);
+
+        assertThat(photoRepository.countByAlbum_AlbumId(savedAlbum.getAlbumId())).isEqualTo(1);
     }
 }
