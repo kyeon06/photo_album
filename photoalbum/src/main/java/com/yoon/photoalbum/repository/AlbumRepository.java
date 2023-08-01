458073f948df103ccaa13cfe24887914c02812c2
kyeon06 <jkyeon06@gmail.com>
2023-08-02 01:29:04 +0900

    :sparkles: feat : 앨범명 검색 시 반환되는 앨범리스트 method 생성

--- photoalbum/src/main/java/com/yoon/photoalbum/repository/AlbumRepository.java
+++ photoalbum/src/main/java/com/yoon/photoalbum/repository/AlbumRepository.java
@@ -4,11 +4,19 @@ import com.yoon.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
     Optional<Album> findByAlbumName(String name);
+
+    // 앨범명 검색 + 생성날짜 최신순
+    List<Album> findByAlbumNameContainingOrderByCreatedAtDesc(String keyword);
+
+    // 앨범명 검색 + 앨범명 A-Z 정렬
+    List<Album> findByAlbumNameContainingOrderByAlbumNameAsc(String keyword);
+
 
 }
